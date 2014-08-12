a174257f229258b38edfdce736e86195c5ba0ef0
Sam Gladstone <gladston@umich.edu>
2014-08-12 18:53:05 -0400

    Add scoring

--- blackjack.cs
+++ blackjack.cs
@@ -3,133 +3,162 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-public class Card {
+public class Card
+{
     public string suit { get; private set; }
     public int rank { get; private set; }
 
     public Card(string suit, int rank)
-      {
-       this.suit = suit;
-       this.rank = rank;
-      }
-  }
+    {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
 
-public class Hand {
+public class Hand
+{
     public List<Card> hand;
 
     public Hand()
-      {
-       this.hand = new List<Card>();
-      }
+    {
+        this.hand = new List<Card>();
+    }
 
     public void AddCards(List<Card> cards)
-      {
-        foreach (Card card in cards) {
-          this.hand.Add(card);
+    {
+        foreach (Card card in cards)
+        {
+            this.hand.Add(card);
         }
-      }
+    }
 
     public void DisplayHand()
-      {
-        foreach (Card card in this.hand) {
-           Console.WriteLine(card.suit + card.rank);
-         }
-      }
-
-    public int Score() {
-      int sum=0;
-      for (int i=0; i<this.hand.Count; i++) {
-        sum += this.hand[i].rank;
-      }
-      return sum;
-    }
-  }
-
-public class Deck {
-  public List<Card> deck = new List<Card>();
-
-  public IEnumerable<int> ranks = Enumerable.Range(1, 10);
-  public string[] suits = new string[]
-               {"H","S","C","D"};
-
-  public Deck()
     {
-      foreach (string suit in suits) {
-          foreach (int rank in ranks) {
-            deck.Add(new Card(suit, rank));
-          }
+        foreach (Card card in this.hand)
+        {
+            Console.WriteLine(card.suit + card.rank);
         }
     }
 
-  public void DealCard(Hand hand)
+    public int Score()
     {
-      Random random = new Random();
-      int index = random.Next(0,40);
-      List<Card> additions = new List<Card>();
-      additions.Add(this.deck[index]);
-      hand.AddCards(additions);
-      this.deck.RemoveAt(index);
+        int sum = 0;
+        for (int i = 0; i < this.hand.Count; i++)
+        {
+            sum += this.hand[i].rank;
+        }
+        return sum;
+    }
+}
+
+public class Deck
+{
+    public List<Card> deck = new List<Card>();
+
+    public IEnumerable<int> ranks = Enumerable.Range(1, 10);
+    public string[] suits = new string[] { "H", "S", "C", "D" };
+
+    public Deck()
+    {
+        foreach (string suit in suits)
+        {
+            foreach (int rank in ranks)
+            {
+                deck.Add(new Card(suit, rank));
+            }
+        }
     }
 
-  public void DealHand(Hand hand)
+    public void DealCard(Hand hand)
     {
-      for(int i=0; i<2; i++ ) {
-        this.DealCard(hand);
-      }
+        Random random = new Random();
+        int index = random.Next(0, deck.Count);
+        List<Card> additions = new List<Card>();
+        additions.Add(this.deck[index]);
+        hand.AddCards(additions);
+        this.deck.RemoveAt(index);
     }
- }
 
-public class GameLogic {
-  public void CompareScores (int PlayerScore, int DealerScore) {
-    if (PlayerScore > 21 || DealerScore > 21) {
-      string loser = PlayerScore>21 ? "You" : "Dealer";
-      Console.WriteLine("{0} went bust! Cry moar.", loser );
-    } else {
-      Console.WriteLine("fooobaaaar");
+    public void DealHand(Hand hand)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            this.DealCard(hand);
+        }
     }
-  }
+}
+
+public class GameLogic
+{
+    public void CompareScores(int PlayerScore, int DealerScore)
+    {
+        if (PlayerScore > 21 || DealerScore > 21)
+        {
+            string loser = PlayerScore > 21 ? "Player" : "Dealer";
+            Console.WriteLine("{0} went bust! Cry moar.", loser);
+        }
+        else if (PlayerScore == 21 || DealerScore == 21)
+        {
+            string winner = PlayerScore == 21 ? "Player" : "Dealer";
+            Console.WriteLine("Blackjack! {0} wins.", winner);
+        }
+        else if (PlayerScore == DealerScore)
+        {
+            Console.WriteLine("Push!");
+        }
+        else // Neither player hits 21 points
+        {
+            string winner = PlayerScore > DealerScore ? "Player" : "Dealer";
+            Console.WriteLine("{0} wins!", winner);
+        }
+    }
 }
 
 
-public class Blackjack {
-  static public void Main ()
-  {
-    GameLogic gamelogic = new GameLogic();
-    string gamestate = "new";
+public class Blackjack
+{
+    static public void Main()
+    {
+        GameLogic gameLogic = new GameLogic();
+        string gamestate = "new";
 
-    while (gamestate != "over") {
-      Deck deck1 = new Deck();
-      string action;
+        while (gamestate != "over")
+        {
+            Deck deck1 = new Deck();
+            string action;
 
-      if (gamestate == "new") {
-        Console.WriteLine("New game!");
+            if (gamestate == "new")
+            {
+                Console.WriteLine("New game!");
 
-        gamestate = "playing";
+                gamestate = "playing";
 
-      } else if (gamestate == "playing") {
+            }
+            else if (gamestate == "playing")
+            {
+                Hand playerHand = new Hand();
+                Hand dealerHand = new Hand();
 
-        Hand playerHand = new Hand();
-        Hand dealerHand = new Hand();
+                deck1.DealHand(playerHand);
+                deck1.DealHand(dealerHand);
 
-        deck1.DealHand(playerHand);
-        deck1.DealHand(dealerHand);
+                Console.WriteLine("You were dealt: ");
+                playerHand.DisplayHand();
 
-        Console.WriteLine("You were dealt: ");
-        playerHand.DisplayHand();
+                action = Console.ReadLine();
 
-        action = Console.ReadLine();
+                if (action == "H")
+                {
+                    deck1.DealCard(playerHand);
+                    Console.WriteLine("Your hand is: ");
+                    playerHand.DisplayHand();
+                    Console.WriteLine("Your score is: ");
+                    Console.WriteLine(playerHand.Score());
+                    gameLogic.CompareScores(playerHand.Score(), dealerHand.Score());
+                }
 
-        if (action == "H") {
-          deck1.DealCard(playerHand);
-          Console.WriteLine("Your hand is: ");
-          playerHand.DisplayHand();
-          Console.WriteLine("Your score is: ");
-          Console.WriteLine(playerHand.Score());
-          GameLogic.CompareScores(playerHand.Score(),dealerHand.Score());
+                gamestate = "over";
+            }
         }
-
-        gamestate = "over";
-      }
     }
-  }
 }
